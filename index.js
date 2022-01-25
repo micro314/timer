a38167d194c61cbbd0b37f9291f51d847fd37216
Michael Rosati <rosati.michael@gmail.com>
2022-01-25 16:35:33 -0500

    Wire onTick and onComplete. Format using Prettier.

--- index.js
+++ index.js
@@ -1,57 +1,65 @@
 class Timer {
-    constructor(durationInput, startButton, pauseButton, callbacks) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
-        if (callbacks) {
-            this.onStart = callbacks.onStart;
-        }
-
-        this.startButton.addEventListener('click', this.start);
-        this.pauseButton.addEventListener('click', this.pause);
+  constructor(durationInput, startButton, pauseButton, callbacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
     }
 
-    start = () => {
-        if (this.onStart)
-            this.onStart();
+    this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
+  }
 
-        this.tick();
-        this.intervalId = setInterval(this.tick, 1000);
-    }
+  start = () => {
+    if (this.onStart) this.onStart();
 
-    pause = () => {
-        clearInterval(this.intervalId);
-    }
+    this.tick();
+    this.intervalId = setInterval(this.tick, 1000);
+  };
 
-    tick = () => {
-        if (this.timeRemaining <= 0) 
-            this.pause();
-        else
-            this.timeRemaining = this.timeRemaining - 1;
-    }
+  pause = () => {
+    clearInterval(this.intervalId);
+  };
 
-    // DOM-centric approach to data storage, done for educational purposes
-    get timeRemaining() {
-        return parseFloat(this.durationInput.value);
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      if (this.onTick) {
+        this.onTick();
+      }
+      this.timeRemaining = this.timeRemaining - 1;
     }
+  };
 
-    set timeRemaining(time) {
-        this.durationInput.value = time;
-    }
+  // DOM-centric approach to data storage, done for educational purposes
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
 }
 
-const durationInput = document.querySelector('#duration');
-const startButton = document.querySelector('#start');
-const pauseButton = document.querySelector('#pause');
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(durationInput, startButton, pauseButton, { 
-    onStart() {
-        console.log('Timer starting')
-    },
-    onPause() {
-
-    },
-    onComplete() {
-
-    }
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log("Timer starting");
+  },
+  onTick() {
+    console.log("Timer ticked");
+  },
+  onComplete() {
+    console.log("Timer complete");
+  },
 });
