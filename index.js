674ae9a771768abb5d48117136d410e169e9d8ad
Michael Rosati <rosati.michael@gmail.com>
2022-01-25 15:31:30 -0500

    Add basic controls to html. Wire start button to stub click handler.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+class Timer {
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click', this.start);
+    }
+
+    start() {
+        console.log('Starting the timer');
+    }
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
